5813cf474424f0463bc081454ee8b2f14c442c45
Chinmay Limje <chinmay.limje@expleogroup.com>
2023-01-13 16:52:10 +0530

    Interactive Rating Thank you page with rating added

--- practice/src/Components/Interactive Rating/Index.jsx
+++ practice/src/Components/Interactive Rating/Index.jsx
@@ -3,72 +3,81 @@ import './style.css'
 import './responsive.css'
 import {Container, Row, Col} from 'react-bootstrap'
 import starIcon from './assets/images/icon-star.svg'
+import Thankyou from './Thankyou'
 
 
 export default function Index() {
 
-  const [active, setActive] = useState(0);
-  const [view, setView] = useState(false);
+  let [value, setValue] = useState();
+  let [active, setActive] = useState(false);
 
-  function handleActive(value){
-    document.getElementById(value).style.background = "hsl(217, 12%, 63%)";
-    document.getElementById(value).style.color = "white";
-
-    if(active != 0){
-      document.getElementById(active).style.color = "white";
-    }
-
-    setActive(value)
-
+  function handleChange(e){
+      setValue(e.target.value);
   }
 
-  function handleSubmit(){
-      console.log(active);
+  function submitRating(e){
+      e.preventDefault();
+      console.log(value);
+      setActive(true)
   }
 
 
-  useEffect(()=>{
-    //console.log(active);
-  })
+  // useEffect(()=>{
+  //   //console.log(active);
+  // })
 
+  if(active == false){
+    return (
+
+
+      <div className="RatingBox">    
+      <Container>
+  
+          <div className="starBox">
+              <img src={starIcon}/>
+          </div>
+  
+          <h3>How did we do?</h3>
+          <p className='desc'>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+  
+          <br />
+  
+          <div className="Ratings">
+              <Row>
+                  {/*<Col> <span className="value" onClick={e=>handleActive(1)} id="1">1</span></Col>
+                  <Col> <span className="value" onClick={e=>handleActive(2)} id="2">2</span></Col>
+                  <Col> <span className="value" onClick={e=>handleActive(3)} id="3">3</span></Col>
+                  <Col> <span className="value" onClick={e=>handleActive(4)} id="4">4</span></Col>
+                  <Col> <span className="value" onClick={e=>handleActive(5)} id="5">5</span></Col>*/}
+              </Row> 
+              <form>
+              <div>
+                  <input type="radio" name="rating" value={1} label="1" onChange={handleChange}/>
+                  <input type="radio" name="rating" value={2} label="2" onChange={handleChange}/>
+                  <input type="radio" name="rating" value={3} label="3" onChange={handleChange}/>
+                  <input type="radio" name="rating" value={4} label="4" onChange={handleChange}/>
+                  <input type="radio" name="rating" value={5} label="5" onChange={handleChange}/>
+              </div>
+              <br />
+              <br />
+  
+              <button onClick={submitRating}>SUBMIT</button>
+                
+              </form>
+              
+          </div>
+  
+          
+  
+      </Container>
+      </div>
+    )
+    
+  }else{
+    return(
+    <Thankyou rating = {value}/>
+    )
+  }
   
   
-  return (
-    <div className="RatingBox">    
-    <Container>
-
-        <div className="starBox">
-            <img src={starIcon}/>
-        </div>
-
-        <h3>How did we do?</h3>
-        <p className='desc'>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-
-        <br />
-
-        <div className="Ratings">
-            <Row>
-                {/*<Col> <span className="value" onClick={e=>handleActive(1)} id="1">1</span></Col>
-                <Col> <span className="value" onClick={e=>handleActive(2)} id="2">2</span></Col>
-                <Col> <span className="value" onClick={e=>handleActive(3)} id="3">3</span></Col>
-                <Col> <span className="value" onClick={e=>handleActive(4)} id="4">4</span></Col>
-                <Col> <span className="value" onClick={e=>handleActive(5)} id="5">5</span></Col>*/}
-            </Row> 
-            <div>
-                <input type="radio" name="rating" value={1} label="1"/>
-                <input type="radio" name="rating" value={2} label="2"/>
-                <input type="radio" name="rating" value={3} label="3"/>
-                <input type="radio" name="rating" value={4} label="4"/>
-                <input type="radio" name="rating" value={5} label="5"/>
-            </div>
-        </div>
-
-        <br />
-        <br />
-
-        <button type="submit" onClick={handleSubmit}>SUBMIT</button>
-
-    </Container>
-    </div>
-  )
 }
--- practice/src/Components/Interactive Rating/Thankyou.jsx
+++ practice/src/Components/Interactive Rating/Thankyou.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import './style.css'
+import {Container, Row, Col} from 'react-bootstrap'
+import topImg from './assets/images/thank-you.svg'
 
-export default function Thankyou() {
+export default function Thankyou(props) {
   return (
-    <div>Thankyou</div>
+    <div className="ThankyouBox">    
+    <Container>
+        <img src={topImg} alt="" />
+        <br />
+
+        <div className="content">
+          <div className="ratings">
+            <p>You Selected {props.rating} out of 5</p>
+          </div>
+
+          <h3>Thank you!</h3>
+          <br />
+          <p>We Appreciate you taking the time to give a rating.<br />if you ever need more support, don't hesistate to get in touch</p>
+
+        </div>
+
+        
+    </Container>
+    </div>
   )
 }
